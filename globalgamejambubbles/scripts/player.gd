8922b2e07e9d40e83e231c91fc760a6e36668cc3
Jack Hannan <jackhannan2k353@gmail.com>
2025-01-21 14:52:50 -0500

    Add tileset

--- globalgamejambubbles/scripts/player.gd
+++ globalgamejambubbles/scripts/player.gd
@@ -3,10 +3,14 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+
 @onready var upper_body = $UpperBody
 
 func _physics_process(delta):
+	# Look at mouse (if mouse mode toggled)
 	upper_body.look_at(get_global_mouse_position())
+	print(get_global_mouse_position())
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -15,12 +19,12 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	# Reset horizontal velocity
+	velocity.x = 0
+	# Handle movement
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1 * SPEED
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1 * SPEED
 
 	move_and_slide()
